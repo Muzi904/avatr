cf80380547986fc8cc7aefd9e9be7d56603c5285
Muzi904 <muhsin@syntaxinnovations.com>
2024-12-04 12:31:39 +0530

    avatr home page correction

--- resources/views/website/layouts/components/head.blade.php
+++ resources/views/website/layouts/components/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Network</title>
+    <title>AVATR</title>
     <link rel="icon" type="image/png" href="{{ asset('website/images/favicon3.png') }}">
     <link rel="stylesheet" href="{{ asset('website/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('website/css/home.css') }}">
