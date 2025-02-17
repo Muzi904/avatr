1375aa648a6236c7a9f6ba29ced11c143bed23ff
Sandeep B <sandeepbiju123@gmail.com>
2025-02-17 16:58:25 +0530

    minor updat ein admin pannel

--- resources/views/admin/pages/enquiries/index.blade.php
+++ resources/views/admin/pages/enquiries/index.blade.php
@@ -9,7 +9,7 @@
             <div class="card-header  d-flex">
                 <div class="justify-content-end">
                     <button type="button" class="btn btn-primary m-2" id="searchModal">Search </button>
-                    <button type="submit" class="btn btn-info m-2" form="export-form">Export</button>
+                    {{-- <button type="submit" class="btn btn-info m-2" form="export-form">Export</button> --}}
                 </div>
             </div>
             <div class="table-responsive text-nowrap">
