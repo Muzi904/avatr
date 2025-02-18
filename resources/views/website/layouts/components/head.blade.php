524679acfcacbe4a3605a5caf1c3473e32f020c5
Sandeep B <sandeepbiju123@gmail.com>
2025-02-18 11:59:37 +0530

    add meta option in header

--- resources/views/website/layouts/components/head.blade.php
+++ resources/views/website/layouts/components/head.blade.php
@@ -1,7 +1,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AVATR</title>
+    @php
+        $slug = last(request()->segments());
+    @endphp
+
+    @if (!isset($slug))
+        <title>AVATR Home</title>
+        <meta name="title" content="home" />
+        <meta name="description" content="home" />
+        <meta name="keywords" content="home">
+    @else
+        @if ($slug == 'avatr11')
+            {{--  --}}
+        @elseif ($slug == 'avatr12')
+            {{--  --}}
+        @elseif ($slug == 'about')
+            {{--  --}}
+        @elseif ($slug == 'contact')
+            {{--  --}}
+        @elseif ($slug == 'test-drive')
+            {{--  --}}
+        @elseif ($slug == 'models')
+            {{--  --}}
+        @elseif ($slug == 'showrooms')
+            {{--  --}}
+        @elseif ($slug == 'invitation')
+            {{--  --}}
+        @endif
+    @endif
+
     <link rel="icon" type="image/png" href="{{ asset('website/images/favicon3.png') }}">
     <link rel="stylesheet" href="{{ asset('website/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('website/css/home.css') }}">
@@ -11,5 +39,6 @@
     <link rel="stylesheet" href="{{ asset('website/css/responsive.css') }}">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" rel="stylesheet">
+
 
 </head>
--- resources/views/website/layouts2/components/head.blade.php
+++ resources/views/website/layouts2/components/head.blade.php
@@ -1,7 +1,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AVATR</title>
+    @php
+        $slug = last(request()->segments());
+    @endphp
+
+    @if (!isset($slug))
+        <title>AVATR Home</title>
+        <meta name="title" content="home" />
+        <meta name="description" content="home" />
+        <meta name="keywords" content="home">
+    @else
+        @if ($slug == 'avatr11')
+            {{--  --}}
+        @elseif ($slug == 'avatr12')
+            {{--  --}}
+        @elseif ($slug == 'about')
+            {{--  --}}
+        @elseif ($slug == 'contact')
+            {{--  --}}
+        @elseif ($slug == 'test-drive')
+            {{--  --}}
+        @elseif ($slug == 'models')
+            {{--  --}}
+        @elseif ($slug == 'showrooms')
+            {{--  --}}
+        @elseif ($slug == 'invitation')
+            {{--  --}}
+        @endif
+    @endif
+
+
     <link rel="icon" type="image/png" href="{{ asset('website/images/favicon3.png') }}">
     <link rel="stylesheet" href="{{ asset('website/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('website/css/home.css') }}">
