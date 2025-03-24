749b48e0995c8e551fba49222a7fd3e67e2b0d4e
Muzi904 <muhsin@syntaxinnovations.com>
2025-03-24 12:31:23 +0530

    changes

--- resources/views/website/layouts/components/foot.blade.php
+++ resources/views/website/layouts/components/foot.blade.php
@@ -245,7 +245,7 @@
         var input = document.querySelector("#country");
         var iti = window.intlTelInput(input, {
             initialCountry: "qa",
-            preferredCountries: ["qa", "ae", "in", "us", "gb"],
+            preferredCountries: ["qa"],
             separateDialCode: true,
             utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"
         });
